c9593e75d97da2e3145b3f6753062739cfcf332a
Rameshmythri <100982758+Rameshmythri@users.noreply.github.com>
2022-09-08 12:49:13 +0530

    welcome lalit

---
+++ src/com/nt/lalit/Welcome_Lalit.java
@@ -0,0 +1,15 @@
+package com.nt.lalit;
+
+public class Welcome_Lalit {
+
+	public Welcome_Lalit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Lalit");
+
+	}
+
+}
